1523ee580157847f84f7c662176c662db5e36f63
yejichun123 <yejiowl@gmail.com>
2020-06-26 17:45:48 +0900

    footer구조

--- js/index.js
+++ js/index.js
@@ -7,7 +7,36 @@
 
 
 /**************************** 이벤트 콜백 ***************************/
+function onResize() {
+  var wid = $(this).innerWidth();
+  
+  $(".mo-wrap").hide();
+  
+  if(wid > 991) {
+  
+  }
+  else if(wid > 767) {
 
+  }
+  else if(wid > 576) {
+
+  }
+  else {
+
+  }
+}
+
+function onScroll() {
+
+}
+
+function onBarsClick() {
+  $(".mo-wrap").stop().slideToggle(300);
+}
 
 
 /**************************** 이벤트 등록 ***************************/
+$(window).resize(onResize).trigger("resize");
+$(window).scroll(onScroll).trigger("scroll");
+
+$(".mo-bars").click(onBarsClick);
